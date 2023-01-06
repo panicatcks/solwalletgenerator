bb9648ff03109eccd7a2b5af43cc381fa90e80bc
panicatcks <60323178+panicatcks@users.noreply.github.com>
2023-01-06 14:46:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,24 @@ class WalletGenerator():
             self.words = self.mnemo.generate(strength=128)
             with open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8') as mnem:
                 mnem.write(f'{self.words}\n')
+    def all(self):
+        self.numfile()
+        for i in range(self.number):
+            self.mnemo = Mnemonic("english")
+            self.words = self.mnemo.generate(strength=128)
+            self.seed = self.mnemo.to_seed(self.words)
+            self.keypair = Keypair.from_seed(hashlib.sha256(self.words.encode("utf-8")).digest())
+            self.keypair = b58encode(self.keypair.secret_key).decode("utf-8")
+            self.pb = Keypair.from_secret_key(base58.b58decode(self.keypair))
+            self.pb = format(self.pb.public_key)
+            with open(f'{self.filename}_privatekeys.txt','a', encoding='utf-8') as file:
+                file.write(f'{self.keypair}\n')
+            with open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8') as mnem:
+                mnem.write(f'{self.words}\n')
+            with open(f'{self.filename}_public.txt','a',encoding='utf-8') as pb:
+                pb.write(f'{self.pb}\n')
     def choose(self):
-        print('K.Keys\nM.Mnemonic\nP.Public\nE.Exit')
+        print('K.Keys\nM.Mnemonic\nP.Public\nA.ALL\nE.Exit')
         choose = input('What do you want generate? ').lower()
         if choose == 'k':
             self.privatekeys()
@@ -43,6 +59,8 @@ class WalletGenerator():
             self.mnemonicphrase()
         elif choose == 'p':
             self.public()
+        elif choose == 'a':
+            self.all()
         elif choose == 'e':
             pass
         else:
