f10d5bf9e8a1694934346815550d27be31041934
panicatcks <60323178+panicatcks@users.noreply.github.com>
2023-01-06 20:01:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ class WalletGenerator():
             self.seed = self.mnemo.to_seed(self.words)
             self.keypair = Keypair.from_seed(hashlib.sha256(self.words.encode("utf-8")).digest())
             self.keypair = b58encode(self.keypair.secret_key).decode("utf-8")
-            with open(f'{self.filename}_privatekeys.txt','a', encoding='utf-8') as file:
-                file.write(f'{self.keypair}\n')
+            self.privatekeyys = open(f'{self.filename}_privatekeys.txt','a', encoding='utf-8')
+            self.privatekeyys.write(f'{self.keypair}\n')
     def public(self):
         self.numfile()
         for i in range(self.number):
@@ -25,15 +25,15 @@ class WalletGenerator():
             self.keypair = b58encode(self.keypair.secret_key).decode("utf-8")
             self.pb = Keypair.from_secret_key(base58.b58decode(self.keypair))
             self.pb = format(self.pb.public_key)
-            with open(f'{self.filename}_public.txt','a',encoding='utf-8') as pb:
-                pb.write(f'{self.pb}\n')
+            self.publicc = open(f'{self.filename}_public.txt','a',encoding='utf-8')
+            self.publicc.write(f'{self.pb}\n')
     def mnemonicphrase(self):
         self.numfile()
         for i in range(self.number):
             self.mnemo = Mnemonic("english")
             self.words = self.mnemo.generate(strength=128)
-            with open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8') as mnem:
-                mnem.write(f'{self.words}\n')
+            self.mnemonicc = open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8')
+            self.mnemonicc.write(f'{self.words}\n')
     def all(self):
         self.numfile()
         for i in range(self.number):
@@ -44,12 +44,12 @@ class WalletGenerator():
             self.keypair = b58encode(self.keypair.secret_key).decode("utf-8")
             self.pb = Keypair.from_secret_key(base58.b58decode(self.keypair))
             self.pb = format(self.pb.public_key)
-            with open(f'{self.filename}_privatekeys.txt','a', encoding='utf-8') as file:
-                file.write(f'{self.keypair}\n')
-            with open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8') as mnem:
-                mnem.write(f'{self.words}\n')
-            with open(f'{self.filename}_public.txt','a',encoding='utf-8') as pb:
-                pb.write(f'{self.pb}\n')
+            self.privatekeyys = open(f'{self.filename}_privatekeys.txt','a', encoding='utf-8')
+            self.privatekeyys.write(f'{self.keypair}\n')
+            self.mnemonicc = open(f'{self.filename}_mnemonic.txt','a', encoding='utf-8')
+            self.mnemonicc.write(f'{self.words}\n')
+            self.publicc = open(f'{self.filename}_public.txt','a',encoding='utf-8')
+            self.publicc.write(f'{self.pb}\n')
     def choose(self):
         print('K.Keys\nM.Mnemonic\nP.Public\nA.ALL\nE.Exit')
         choose = input('What do you want generate? ').lower()
@@ -62,7 +62,13 @@ class WalletGenerator():
         elif choose == 'a':
             self.all()
         elif choose == 'e':
-            pass
+            try:
+                self.privatekeyys.close()
+                self.mnemonicc.close()
+                self.publicc.close()
+                pass
+            except:
+                pass
         else:
             print('-'*30)
             print('Wrong choose')
